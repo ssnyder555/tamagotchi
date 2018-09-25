1ca6bd11333c5e3829e042c94c5bd815f34522d8
Stephen Snyder <ssnyder555@live.com>
2018-09-25 16:43:07 -0500

    skelton done

---
+++ script/app.js
@@ -0,0 +1,11 @@
+console.log('working');
+
+var person = prompt("Please enter your Pet's name", "");
+
+
+
+// $(‘button’).on(‘click’, () => {
+//  const $name = $(‘input’).val();
+//  console.log($name);
+//  giveName($name)
+// })
