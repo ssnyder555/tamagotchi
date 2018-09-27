17890605914b2ffd20972e06909dcc950505ed73
Stephen Snyder <ssnyder555@live.com>
2018-09-26 22:44:54 -0500

    almost finished up

--- script/app.js
+++ script/app.js
@@ -1,25 +1,27 @@
 
 
-//-----Name-------
-let time = 0;
-//----Tamagotchi class
-class tamagotchi {
- constructor(hunger, sleepiness, boredom, age) {
-   this.hunger = 10;
-   this.sleepiness = 10;
-   this.boredom = 10;
-   this.age = 0;
-   this.name = name;
- }
-};
 
+//Global Variables
+let ageHealth = $('#age').val()
+let time = 0;
+let healthBarsMin = 0
+let healthBarsMax = 10
+let timerInterval = 5000
 //------Register name on screen after input box------
-   $('#monster').on('click', () => {
-    const userName = $("#userNameInput").val();
-    $("#userDisplay").html(userName);
-      console.log('click workin');
-      console.log(userName);
-});
+
+
+//Name Functions
+$('#nameButton').on('click', () => {
+  const $name = $('input').val();
+  console.log($name);
+  giveName($name)
+})
+
+const giveName = (inputValue) => {
+  const $div = $('<div/>').addClass('greeting');
+  $div.append('<h3>' + 'Hi ' + inputValue + ' !' + '</h3>');
+  $('.home').append($div);
+}
 // Increase your pet's age every x minutes
 // Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
 
@@ -44,32 +46,163 @@ $('.play').on('click', () => {
   $('.evilness').val(evilnessVal);
 })
 
-// const setTimer = () => {
-//   const timer = setInerval () =>
-//     time--;
-//     if (time % 10 === 0) {
-//       age += 10;
-//       console.log(age);
-//     }
-//   }
-// }
 
-const setTimer = () => {
- const timer = setInterval(function() {
-   console.log(time);
-   time--;
+//Min & Max Function
+function checkMinAndMax(healthBar) {
+  if (healthBar.val() <= healthBarsMin) {
+    healthBar.val(healthBarsMin)
+  }
+    if (healthBar.val() >= healthBarsMax) {
+    healthBar.val(healthBarsMax)
+  }
+}
 
-   if(time === 0) {
-     clearInterval(timer);
-   }
- }, 2000);
+//Created Game Over Functions
+function checkHunger(healthBar) {
+  if ($('#hunger').val() === 0) {
+    console.log('hunger game over');
+    // alert('GAME OVER')
+  }
+}
+
+function checkEnergy(healthBar) {
+  if ($('#sleep').val() === 0) {
+    console.log('energy game over');
+  //  alert('GAME OVER')
+  }
+}
+
+function checkMood(healthBar) {
+  if ($('#mood').val() === 0) {
+    //console.log('your pet killed himself');
+  //  alert('GAME OVER')
+  }
 };
-
-
-$('.startButton').on('click', () => {
- setTimer();
+//Button click event listeners
+$('#play').on('click', function() {
+  incrementHealthBar($('#mood'))
+  checkMinAndMax($('#mood'))
+  $('.pet').velocity('transition.bounceUpIn');
+  $('.pet').velocity('transition.bounceDownIn');
 })
 
-// $('.startButton').on('click', () => {
-//  setTimer();
-// })
+$('#feed').on('click', function() {
+  incrementHealthBar($('#hunger'))
+  checkMinAndMax($('#hunger'))
+})
+
+$('#lights').on('click', function() {
+  incrementHealthBar($('#sleep'))
+  checkMinAndMax($('#sleep'))
+  $('.eyes').velocity('transition.shrinkOut');
+  $('.eyes').velocity('transition.shrinkIn');
+})
+
+
+//Animations
+$('.pet').velocity('transition.bounceDownIn');
+$('.pet').velocity('transition.bounceUpIn');
+//$('.eyes').velocity('transition.shrinkOut');
+
+
+//Utility Functions
+function incrementHealthBar(healthBar) {
+  let health = healthBar.val()
+  health++
+  healthBar.val(health)
+}
+
+function decrementHealthBar(healthBar) {
+  let health = healthBar.val()
+  health--
+  healthBar.val(health)
+}
+
+function incrementAge() {
+  let age = $('#age').val();
+  age++;
+  $('#age').val(age);
+}
+
+//Timer
+const setTimer = (healthBar) => {
+  const timer = setInterval(() => {
+
+    if ($('#hunger').val() === 0 || $('#mood').val() === 0 || $('#sleep').val() === 0) {
+      alert('GAME OVER')
+      clearInterval(timer);
+    }
+
+    decrementHealthBar(healthBar)
+    checkMinAndMax(healthBar);
+}, timerInterval)
+}
+
+//Age Timer
+const ageTimer = (healthBar) => {
+  const timer = setInterval(() => {
+  child()
+  adult()
+  elder()
+  time++;
+
+  incrementAge(healthBar)
+
+  if (time === 40) {
+      clearInterval(timer);
+      console.log('game over');
+    //  alert('GAME OVER')
+    }
+  }, 1000)
+
+}
+
+
+function startTamagotchi() {
+  setTimer($('#mood'));
+  setTimer($('#sleep'));
+  setTimer($('#hunger'));
+  ageTimer($('#age'));
+}
+
+
+
+
+function checkGameOver() {
+  checkHunger();
+  checkMood();
+  checkEnergy();
+}
+
+function child() {
+  if ($('#age').val() === 10) {
+    console.log('hello');
+    $('.pet').velocity('transition.bounceDownIn');
+    $('.pet').css({'width': '300px', 'height': '250px', 'left': '50%', 'top': '60%'})
+  }
+}
+
+function adult() {
+  if ($('#age').val() === 20) {
+    console.log('hello');
+    $('.pet').velocity('transition.bounceDownIn');
+    $('.head').css({'background': 'blue'});
+    $('.body').css({'background': 'blue'});
+    $('.pet').css({'width': '350px', 'height': '300px', 'left': '40%', 'top': '45%'})
+  }
+}
+
+function elder() {
+  if ($('#age').val() === 30) {
+    console.log('hello');
+    $('.pet').velocity('transition.bounceDownIn');
+    $('.pet').css({'width': '280px', 'height': '240px', 'left': '50%', 'top': '60%'})
+    $('.head').css({'background': 'gray'});
+    $('.body').css({'background': 'gray'});
+    $('.eyes span').css({'width': '10%', 'height': '10%'});
+  }
+}
+
+
+
+startTamagotchi()
